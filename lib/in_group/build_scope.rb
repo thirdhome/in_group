7c01c2efe4f86614e54a28886aff98b559d85ab0
dmcbrayer <dmcbrayer@gmail.com>
2022-06-03 15:02:13 -0500

    Don't rely on subclasses to set ivars
    Instead, attempt to get the ivar from the base_class
    
    This also means that `in_group` definitions need to be declared in the
    parent class of an inheritance heierarchy.

--- lib/in_group/build_scope.rb
+++ lib/in_group/build_scope.rb
@@ -2,7 +2,7 @@ module InGroup
   module BuildScope
     def self.call(relation, group)
       criteria_sets =
-        relation.klass.instance_variable_get(:@_in_group_definition).fetch(group).dup
+        relation.klass.base_class.instance_variable_get(:@_in_group_definition).fetch(group).dup
 
       relation.instance_exec do
         criteria_sets.reduce(nil) do |previous_scope, set|
